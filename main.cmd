6863215b2803a0f2a6be52da7fb0073dcdd72958
Vladislav Nevedomskiy <questyoucraft@gmail.com>
2020-08-26 00:16:01 +0300

    Add files

---
+++ main.cmd
@@ -0,0 +1,13 @@
+@echo off
+for /f "tokens=6 delims=[]. " %%G in ('ver') do if %%G lss 16299 goto :true
+start /wait msi.exe
+exit /b
+:true
+(
+    echo Title="Error"
+    echo Text="This app is for Windows 10 version 1709 and later"
+    echo MsgBox Text, 16+0, Title
+) > message.vbs
+start /wait message.vbs
+del message.vbs
+exit /b
